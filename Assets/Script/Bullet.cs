3f64aa6f4fb8bed53a40fd96c1b513ef0fd5bac2
devdoong <devdoong@naver.com>
2024-05-09 22:33:48 +0900

    reset

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -7,6 +7,11 @@ public class Bullet : MonoBehaviour
     public float damage;
     public int per;
 
+    //테스트 여러여러 코드가 쭈루룩 들어갔어요.
+    //
+    //
+    //
+    //
     public void Init(float damage, int per) //per 관통
     {
         this.damage = damage;
